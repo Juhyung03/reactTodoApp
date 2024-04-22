4cf64b5d3c608c47bbda1aac26c9964635b99896
Juhyung03 <njhzi1019@gmail.com>
2024-04-22 16:50:03 +0900

    tailwind css 추가

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import React,{useState} from "react";
+import React,{useState, useCallback} from "react";
 import "./App.css";
-
+import List from "./components/List";
+import Form from "./components/Form";
+const initialTodoData = localStorage.getItem("todoData") ? JSON.parse(localStorage.getItem("todoData")) : [];
 export default function App() {
+  console.log("App.js is rendering")
 
 
-  const [todoData, setTodoData] = useState([]);
+  const [todoData, setTodoData] = useState(initialTodoData);
   const [value, setValue] = useState("");
 
-  const btnStyle = {
-    color:"#fff",
-    border: "none",
-    padding:"5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right"
-  }
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom:"1px #ccc dotted",
-      textDecoration: completed ? "line-through": "none",
-    }
-  }
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter(data=>data.id !== id)
-    console.log('newTodoData',newTodoData)
-    setTodoData(newTodoData)
-
-  }
-
-  const handleChange = (e) => {
-    setValue(e.target.value)
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -46,54 +22,39 @@ export default function App() {
 
     //this.setState({ todoData : [...todoData, newTodo], value:""})
     setTodoData(prev => [...prev, newTodo])
+    localStorage.setItem('todoData',JSON.stringify([...todoData, newTodo]))
     setValue("")
     //Setter에서 이전 State를 가지고 오기 위해서는 함수 이용
   }
 
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map(data=>{
-      if(data.id === id){
-        data.completed = !data.completed;
-      }
-      return data;
-    })
-
-    
+  const handleClick = useCallback((id) => {
+    let newTodoData = todoData.filter(data=>data.id !== id)
+    console.log('newTodoData',newTodoData)
     setTodoData(newTodoData)
+    localStorage.setItem('todoData',JSON.stringify(newTodoData))
+
+  },[todoData]);
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem('todoData',JSON.stringify([]))
   }
   
     return(
 
-      <div className="container"> 
+      <div className="flex items-center justify-center w-screen h-screen bg-blue-100" > 
       {/* JSX에서는 className  */}
-        <div className="todoBlock">
-          <div className="title">
+        <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+          <div className="flex justify-between mb-3">
             <h1>할 일 목록</h1>
+            <button onClick={handleRemoveClick}>Delete All</button>
           </div>
-
-          {todoData.map(data=>(
-            <div style={getStyle(data.completed)} key={data.id}>
-            <input type="checkbox" defaultChecked={data.completed} onChange={()=>handleCompleteChange(data.id)}/>
-            {data.title}
-            <button style={btnStyle} onClick={()=>handleClick(data.id)}>x</button>
-          </div>
-
-          ))}
-
-          <form style={{display: 'flex'}} onSubmit={handleSubmit}>
-            <input type="text" name="value" style={{flex:'10', padding: '5px'}}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-            />
-            <input type="submit"
-            value="입력"
-            className="btn"
-            style={{flex:'1'}}
-            />
-          </form>
-
           
+
+          <List todoData = {todoData} setTodoData = {setTodoData} handleClick={handleClick}/>
+
+          <Form value = {value} setValue = {setValue} handleSubmit = {handleSubmit}/>
+
 
         </div>
         
